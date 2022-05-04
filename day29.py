f3c6cf87da4ac2a034236c18b9107394456f77e5
Chephos <efosacharlesabu@gmail.com>
2022-05-04 22:05:32 +0100

    sudoku-solver

--- day29.py
+++ day29.py
@@ -1,40 +1,46 @@
 
-def gpa_calculator(result:dict):
+def gpa_calculator(result:list)->float:
     """Returns the GPA of the student."""
     total_credit_unit = 0
     total_quality_point = 0
 
-    for grade,credit_unit in result.items():
+    for grade,credit_unit in result:
+            
+
         total_credit_unit += credit_unit
 
         if grade.upper() == 'A':
             total_quality_point += (credit_unit*5)  
-            print(total_quality_point)
+                    
 
         elif grade.upper() == 'B':
             total_quality_point += (credit_unit*4)  
-            print(total_quality_point)
-            
+                    
+
         elif grade.upper() == 'C':
             total_quality_point += (credit_unit*3)  
-            print(total_quality_point)
+                    
 
         elif grade.upper() == 'D':
             total_quality_point += (credit_unit*2)  
-            print(total_quality_point)
+                    
 
         elif grade.upper() == 'E':
             total_quality_point += (credit_unit*1)  
-            print(total_quality_point)
-
+                    
         elif grade.upper() == 'F':
             total_quality_point += (credit_unit*0) 
-            print(total_quality_point)
+            
 
     gpa = float((total_quality_point)/(total_credit_unit))
-    print(total_quality_point,total_credit_unit)
+    
 
-    return gpa
+    return round(gpa,2)
 
-student1 = {'B':3,'C':2,'C':1,'b':4,'a':5,'d':2}
+#the input should be in the format [(grade,credit unit),(grade,i.e"A" or"B".,credit unit)...]
+student1 = [('B',3),('C',2,),('c',1),('b',4),('a',5),('d',2)]
+# for v,i in student1:
+#     print(v.upper(),i)
+
+
 print(f'GPA: {gpa_calculator(student1)}')
---
+++ day30.py
@@ -0,0 +1,80 @@
+
+
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row,col = find
+
+    for i in range(1,10):
+        if valid(bo,i,(row,col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+def valid(bo:list,num:int,pos:tuple):
+
+    # Check row
+    for col in range(len(bo[0])):
+        if bo[pos[0]][col] == num and pos[1]!=col:
+            return False
+
+    # Check column
+    for row in range(len(bo)):
+        if bo[row][pos[1]] == num and pos[0] != row:
+            return False
+
+    
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3,box_y*3+3):
+        for j in range(box_x*3,box_x*3+3):
+            if bo[i][j]==num and (i,j)!=pos:
+                return False
+
+    return True
+
+
+def print_board(bo):
+    for r in range(len(bo)):
+        if r%3==0 and r != 0:
+            print('-  -  -  -  -  -  -  -  -  -  -')
+        for c in range(len(bo[r])):
+            if c % 3==0 and c!= 0:
+                print(' | ',end='')
+            if c == 8:
+                print(bo[r][c])
+            else:
+                print(f'{bo[r][c]} ',end=' ')
+
+def find_empty(bo):
+    for row in range(len(bo)):
+        for col in range(len(bo[row])):
+            if bo[row][col] == 0:
+                return (row,col)
+    return False
+
+
+print_board(board)
+solve(board)
+print('----------------------------------')
+print_board(board)
